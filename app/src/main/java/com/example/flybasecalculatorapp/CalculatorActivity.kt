e7b2ec40933e01bdde83621cbe9986d287a53a16
ramanuj18 <kesharawaniramanuj@gmail.com>
2021-05-14 18:06:33 +0530

    ui designing and handle input of calculator buttons

--- app/src/main/java/com/example/flybasecalculatorapp/CalculatorActivity.kt
+++ app/src/main/java/com/example/flybasecalculatorapp/CalculatorActivity.kt
@@ -1,22 +1,40 @@
 package com.example.flybasecalculatorapp
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.flybasecalculatorapp.databinding.ActivityCalculatorBinding
 
 
 class CalculatorActivity : AppCompatActivity() {
 
-    private lateinit var viewModel: ViewModel
+    private lateinit var viewModel: CalculatorViewModel
     private lateinit var binding: ActivityCalculatorBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_calculator)
+        binding.handler = this
 
         viewModel = ViewModelProvider(this).get(CalculatorViewModel::class.java)
+
+        binding.model = viewModel
+
+        viewModel.rawCalculationString.observe(this, Observer {value ->
+            value?.let { binding.editText.setText(it)}
+        })
+
+        viewModel.errorMessage.observe(this, Observer {error ->
+            error?.let { Toast.makeText(this@CalculatorActivity, error, Toast.LENGTH_SHORT).show() }
+        })
+    }
+
+    fun onButtonClick(view: View,value: String){
+        viewModel.validateInput(value)
     }
 }
--- app/src/main/java/com/example/flybasecalculatorapp/CalculatorViewModel.kt
+++ app/src/main/java/com/example/flybasecalculatorapp/CalculatorViewModel.kt
@@ -1,11 +1,38 @@
 package com.example.flybasecalculatorapp
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 /**
  * created by Ramanuj Kesharawani on 14/5/21
  */
-class CalculatorViewModel: ViewModel(){
+class CalculatorViewModel : ViewModel() {
+
+    private val _rawCalculationString = MutableLiveData<String>("")
+    val rawCalculationString: LiveData<String>
+        get() = _rawCalculationString
+
+    private val _errorMessage = MutableLiveData<String>()
+    val errorMessage: LiveData<String>
+        get() = _errorMessage
+
+    fun validateInput(string: String) {
+        val prevString = _rawCalculationString.value!!
+        when (string) {
+            "x", "/", "-", "+" -> {
+                if (prevString.isNotEmpty() && prevString[prevString.length - 1].toString().matches("[0-9]+".toRegex())) {
+                    _rawCalculationString.value += string
+                }else{
+                    _errorMessage.value = "Please enter valid input"
+                }
+            }
+            else -> {
+                _rawCalculationString.value += string
+            }
+        }
+
+    }
 
 
 }
